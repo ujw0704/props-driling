5b33d7f3160e68819b804d5dbfcdcacb2d2ae883
ujw0704 <ujwalsharma571@gmail.com>
2023-09-17 22:04:35 +0530

    third commit

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,67 +1,39 @@
-// import React from 'react';
-// import Card from './Card';
-
-// function Cards(props) {
-//   let courses = props.courses;
-// //   console.log("printing data");
-// //   console.log(courses);
-
-//   function getcourses() {
-//     let allcourses = [];
-
-//     Object.values(courses).forEach(array => {
-//       array.forEach(courseData => {
-//         allcourses.push(courseData);
-//     });
-    
-    
-// });
-
-// console.log(allcourses)
-// return allcourses;
-// }
-    
-
-  
-//   return (
-//     <div>
-//      {
-//         getcourses().map((course)=>{
-            
-//             <Card key={course.id}course={course}/>
-//         })
-//      }
-
-//     </div>
-//   );
-// }
-
-// export default Cards;
 import React from 'react';
 import Card from './Card';
 
 function Cards(props) {
   let courses = props.courses;
+//   console.log("printing data");
+//   console.log(courses);
 
   function getcourses() {
     let allcourses = [];
 
-    Object.values(courses).forEach((array) => {
-      array.forEach((courseData) => {
+    Object.values(courses).forEach(array => {
+      array.forEach(courseData => {
         allcourses.push(courseData);
-      });
     });
+    
+    
+});
 
-    return allcourses; // Return the array here
-  }
 
+return allcourses;
+}
+    
+
+  
   return (
     <div>
-      {getcourses().map((course) => (
-        <Card key={course.id} course={course} />
-      ))}
+     {
+        getcourses().map((course)=>(
+            
+            <Card key={course.id}course={course}/>
+        ))}
+     
+
     </div>
   );
 }
 
-export default Cards;
+ export default Cards;
